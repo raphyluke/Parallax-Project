42c6813985ca64f09efc6ace2515b66de5a4295d
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2023-12-10 17:28:36 +0100

    Add content and animation fromTop

--- script.js
+++ script.js
@@ -2,9 +2,29 @@ var parallaxdiv = document.getElementsByClassName("parallax")[0];
 var parallaxtext = document.getElementById("parallax-text");
 var parallaxbutton = document.getElementById("parallax-button");
 
+var textSectionCenter = document.getElementsByClassName("text-section-center")[0];
+var textSectionGrid = document.getElementsByClassName("text-section-grid")[0];
+function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+}
+
 window.addEventListener("scroll", function() {
     var scrolledHeight = window.scrollY;
     parallaxdiv.style.backgroundPositionY = scrolledHeight * 0.05 + "px";
     parallaxtext.style.left =  50 + scrolledHeight * 0.05 + "%";
     parallaxbutton.style.right =  44 + scrolledHeight * 0.05 + "%";
+
+    // when the text section is in the viewport, start the animation "fromTop"
+    if (isInViewport(textSectionCenter)) {
+        textSectionCenter.classList.add("fromTop");
+    }
+    if (isInViewport(textSectionGrid)) {
+        textSectionGrid.classList.add("fromTop");
+    }
 });
+
+
